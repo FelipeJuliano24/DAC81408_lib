8d1066faa589b2854b611b16b16efd624c222403
Felipe <colkiese171@gmail.com>
2024-07-16 10:43:59 -0300

    update main.ino

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+#include <Arduino.h>,
 #include <DAC81408.h>
 
 #define DAC_CS 10
@@ -8,42 +8,54 @@
 DAC81408 dac(DAC_CS, DAC_RST, DAC_LDAC, &SPI, 30000000);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
 
   Serial.println("init");
 
   int res = dac.init();
-  if (res == 0) Serial.println("DAC81408 init success");
+  if (res == 0) Serial.println("DAC81404 init success");
 
   dac.set_int_reference(true);
   Serial.print("internal ref stat = ");
   Serial.println(dac.get_int_reference());
 
-  for (int ch = 1; ch <= 8; ch++) {
-    dac.set_ch_enabled(ch, true);
-  }
+  dac.set_ch_enabled(1, true);
+  dac.set_ch_enabled(2, true);
+  dac.set_ch_enabled(3, true);
+  dac.set_ch_enabled(4, true);
+  dac.set_ch_enabled(5, true);
 
-  for (int ch = 0; ch <= 3; ch++) {
-    dac.set_range(ch, DAC81408::U_10);
+
+  for (int i = 1; i < 5; i++) {
+    Serial.print("ch ");
+    Serial.print(i);
+    Serial.print(" power -> ");
+    Serial.println(dac.get_ch_enabled(i));
   }
+  Serial.println();
+
+  dac.set_range(1, DAC81408::U_5); // 0 -> +10V
+  dac.set_range(2, DAC81408::U_5); // 0 -> +10V
+  dac.set_range(3, DAC81408::U_5); // 0 -> +10V
+  dac.set_range(4, DAC81408::U_5); // 0 -> +10V
+  dac.set_range(5, DAC81408::U_5); // 0 -> +10V
+
+
+  dac.set_out(1, 10);
 }
 
 uint16_t i = 0;
+bool done = true;
 
 void loop() {
-  dac.set_out(0, i % 65535);
-  dac.set_out(1, (2 * i) % 65535);
-  dac.set_out(2, (3 * i) % 65535);
-  dac.set_out(3, (4 * i) % 65535);
+  
+  dac.set_out(0, (i)%65535 );
+  dac.set_out(1, (2*i)%65535 );
+  dac.set_out(2, (3*i)%65535 );
+  dac.set_out(3, (4*i)%65535);
 
-  i = (i + 64) % 65535;
+  i = (i+64)%65535;  
   delayMicroseconds(1);
 
-  if (Serial.available() > 0) {  // Verifica se há dados disponíveis para leitura
-    String dadosRecebidos = Serial.readStringUntil('\n');  // Lê os dados recebidos até encontrar um caractere de nova linha (\n)
-    Serial.print("Dados recebidos: ");
-    Serial.println(dadosRecebidos);  // Imprime os dados recebidos no monitor serial
-  }
 }
-
